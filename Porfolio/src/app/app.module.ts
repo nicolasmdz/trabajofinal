c00a2d9613afef7ec3ad88fb83b793aa7251700f
nicolasmdz <nicolasmonacoojeda@gmail.com>
2022-07-29 02:14:12 +0200

    trabjmos en el componente logo-ap y creamos el componente redes

--- Porfolio/src/app/app.module.ts
+++ Porfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { LogoApComponent } from './component/logo-ap/logo-ap.component';
+import { RedesComponent } from './component/redes/redes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoApComponent
+    LogoApComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
